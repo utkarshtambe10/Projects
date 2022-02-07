d373dbf9e7322bb07f078a9261f17f60c09a536e
Utkarsh Tambe <89902664+utkarshtambe10@users.noreply.github.com>
2022-02-07 15:20:24 +0530

    Delete Simple Portfolio directory

--- Simple Portfolio/Index.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-const navUl = document.getElementById('nav-Ul');
-
-hamburger.addEventListener('click' , () => {
-  navUl.classList.toggle('show');
-});
